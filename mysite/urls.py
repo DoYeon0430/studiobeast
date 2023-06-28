98f0660e7a1bc12c5590ca5c012e68232695cc15
DoYeon0430 <kim73916@naver.com>
2023-06-29 01:53:36 +0900

    settings new8 upload

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,11 +18,14 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.urls import re_path as url
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("about.urls")),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
     path("about/", include("about.urls")),
     path("works/", include("works.urls")),
     path("news/", include("news.urls")),
