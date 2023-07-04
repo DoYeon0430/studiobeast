9124cbb20d325443edcae5195665d2ad0a7f2aa9
DoYeon0430 <kim73916@naver.com>
2023-07-04 11:59:56 +0900

    sitemap commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -39,6 +39,10 @@ urlpatterns = [
 
     #robots.txt
     path('robots.txt',  TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
+
+    #sitemal.xml
+    path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml', content_type='text/xml')),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
