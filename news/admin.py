8d4f2f7b7de3e4a7789f52d818a73ff3873f2090
DoYeon0430 <kim73916@naver.com>
2023-07-10 07:46:52 +0900

    admin renew upload

--- news/admin.py
+++ news/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import News
 
-admin.site.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'author', 'create_date')
+    list_filter = ('create_date',)
+    search_fields = ('subject', 'author')
+    fieldsets = [
+        ('제목 필드', {'fields': ('subject',)}),
+        ('글쓴이 필드', {'fields': ('author',)}),
+        ('내용 필드', {'fields': ('content',)}),
+        ('작성일 필드', {'fields': ('create_date',)}),
+    ]
+
+admin.site.register(News, NewsAdmin)
--- news/models.py
+++ news/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from tinymce.models import HTMLField
 
 class News(models.Model):
-    subject = models.CharField(max_length=100)
-    author = models.CharField(max_length=50)
-    content = HTMLField()
-    create_date = models.DateTimeField()
+    subject = models.CharField(max_length=100, verbose_name='제목')
+    author = models.CharField(max_length=50, verbose_name='글쓴이')
+    content = HTMLField(verbose_name='내용')
+    create_date = models.DateTimeField(verbose_name='작성일')
 
     def __str__(self):
         return self.subject
