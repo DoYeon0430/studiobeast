c7859b2ff490c8c471f4ada0078a67635210dd82
DoYeon0430 <kim73916@naver.com>
2023-07-09 13:36:54 +0900

    works renew4 upload

--- works/models.py
+++ works/models.py
@@ -7,16 +7,17 @@ class Works(models.Model):
         ('contents', 'Contents'),
     ]
 
-    subject = models.CharField(max_length=100)
-    number = models.IntegerField()
-    making = models.CharField(max_length=100)
-    director = models.CharField(max_length=100)
-    produce = models.CharField(max_length=100)
-    actor = models.CharField(max_length=100)
-    create_date = models.CharField(max_length=100)
-    content = models.CharField(max_length=130)
-    photo = models.ImageField(upload_to='works/')
-    condition = models.CharField(max_length=100, choices=CONDITION_CHOICES)
+    subject = models.CharField(max_length=100, verbose_name='제목')
+    making = models.CharField(max_length=100, verbose_name='기획 / 제작')
+    director = models.CharField(max_length=100, verbose_name='감독')
+    produce = models.CharField(max_length=100, verbose_name='프로듀서')
+    actor = models.CharField(max_length=100, verbose_name='주연')
+    create_date = models.CharField(max_length=100, verbose_name='개봉일')
+    content = models.CharField(max_length=130, verbose_name='줄거리')
+    photo = models.ImageField(upload_to='works/', verbose_name='포스터')
+    condition = models.CharField(max_length=100, choices=CONDITION_CHOICES, verbose_name='장르')
+    number = models.IntegerField(verbose_name='배치 순서')
 
     def __str__(self):
         return self.subject
+
