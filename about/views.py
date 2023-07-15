9d18e5b7db813d2603a6b7fab522931603cfacf6
DoYeon0430 <kim73916@naver.com>
2023-07-15 14:05:38 +0900

    poster new upload

--- about/views.py
+++ about/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 from works.models import Works
 
 def index(request):
+    contents_works = Works.objects.filter(condition='contents').order_by('-create_date')
     continue_ott_works = Works.objects.filter(condition='continue_ott').order_by('number')
     continue_movie_works = Works.objects.filter(condition='continue_movie').order_by('number')
 
     context = {
+        'contents_works' : contents_works,
         'continue_ott_works': continue_ott_works,
         'continue_movie_works': continue_movie_works
     }
